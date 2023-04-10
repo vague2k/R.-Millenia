2c093c6c0938291a991b6446bea177c7339fa4af
vague2k <121782036+vague2k@users.noreply.github.com>
2023-04-10 19:52:13 -0400

    Update schema.sql

---
+++ schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS todos(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    owner_id INTEGER NOT NULL,
+    guild_id INTEGER NOT NULL,
+    channel_id INTEGER NOT NULL,
+    message_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    added_at TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS removeLeaderboard(
+    server_id INTERGER, 
+    user_id INTEGER, 
+    count INTEGER, 
+    PRIMARY KEY(server_id, member_id)
+);
