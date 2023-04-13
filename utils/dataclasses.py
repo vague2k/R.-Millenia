4d1d105fd5cfb5a130d491655ad4f976fea69004
vague2k <121782036+vague2k@users.noreply.github.com>
2023-04-13 05:16:48 -0400

    Add dataclass for tickets

--- utils/dataclasses.py
+++ utils/dataclasses.py
@@ -37,3 +37,26 @@ class TodoItem:
     @property
     def added_message_jump_url(self) -> str:
         return f"https://discord.com/channels/{self.guild_id}/{self.channel_id}/{self.message_id}"
+
+
+@dataclass(slots=True)
+class TicketItem:
+    id: int
+    owner_id: int
+    guild_id: int
+    channel_id: int
+    message_id: int
+    content: str
+    added_at: str  # ISO FORMAT DATETIME
+
+    @classmethod
+    def from_row(cls, row: sqlite3.Row, /) -> TicketItem:
+        return cls(**dict(row))
+
+    @property
+    def added_at_dt(self) -> datetime.datetime:
+        return datetime.datetime.fromisoformat(self.added_at)
+
+    @property
+    def added_message_jump_url(self) -> str:
+        return f"https://discord.com/channels/{self.guild_id}/{self.channel_id}/{self.message_id}"
